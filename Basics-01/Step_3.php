669c09e5520737a5f364b9655399ad54347f0c0a
Hussein-dev-prog <Hussein.aref.csci@gmail.com>
2021-05-24 10:49:15 +0300

    Step_3.php

---
+++ Basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php 
+$name = "Hussein";
+$email = "Hussein.csci@gmail.com";
+echo "My name is ".$name." and my email is ".$email ;
+$height = 175;
+$width = 90;
+$area = $height * $width ;
+echo "<br> $area";
+?>
