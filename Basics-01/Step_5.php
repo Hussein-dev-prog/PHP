af84073be2c4bc60541194b5f18d9635571599b8
Hussein-dev-prog <Hussein.aref.csci@gmail.com>
2021-05-24 10:50:23 +0300

    Step_5.php

---
+++ Basics-01/Step_5.php
@@ -0,0 +1,13 @@
+<?php 
+$course_name = "Arduino";
+$enrolled_students = 20;
+$price = 30;
+$on_discount = true ;
+print "
+Course title: $course_name, the language of no bugs
+<br>Enrolled Students: $enrolled_students
+<br>Course price: $price $ USD
+<br>Course on discount: $on_discount
+
+"
+?>
