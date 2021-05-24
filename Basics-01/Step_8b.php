6b5f9692ddf29f2089065dea012181731a84043f
Hussein-dev-prog <Hussein.aref.csci@gmail.com>
2021-05-24 12:26:28 +0300

    Step_8b.php

---
+++ Basics-01/Step_8b.php
@@ -0,0 +1,8 @@
+<?php
+$array = array (
+    array ( 'egss' , 'balade', 'mazere3' ),   
+    array ( 'milk','Fresh', 'Taanayel'), 
+    array ('water pack',  'Tanoureen', 'Reem' )
+);
+echo "Hey Sir, Please I need 1 pack of ".$array[0] [1]." ".$array[0] [0]." and 3 ".$array[2] [2]." of ".$array[2] [0];
+?>
