f5fb3112e3b119af55d256e113f14b5dcdaec3cf
Hussein-dev-prog <Hussein.aref.csci@gmail.com>
2021-05-24 14:08:35 +0300

    Step_3.php

---
+++ Basics-02/Step_3.php
@@ -0,0 +1,16 @@
+<?php
+$var1 = 32;
+$var2 = 45;
+ 
+// Swap Logic
+
+echo "\nThe number before swapping is:\n";
+echo "Number var1 =".$var1." and var2=".$var2 ."<br>";
+
+$temp = $var1;
+$var1 = $var2;
+$var2 = $temp;
+ 
+echo "\nThe number after swapping is: \n ";
+echo "Number var1 =".$var1." and var2=".$var2."\n";
+?>
