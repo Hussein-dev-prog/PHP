4e067ee4da26f562b6cb66124925d5400892c9e7
Hussein-dev-prog <Hussein.aref.csci@gmail.com>
2021-05-24 10:49:47 +0300

    Step_4.php

---
+++ Basics-01/Step_4.php
@@ -0,0 +1,8 @@
+<?php 
+$name = "Hussein";
+$position = "Full Stack web development";
+$github_ur = "https://github.com/Hussein-dev-prog";
+echo "Hello I'm ".$name.",I'm a ".$position." please check my github link  ".$github_ur ;
+echo "<br>Hello I'm $name ,I'm a $position  please check my github link  $github_ur ";
+// echo '<br>Hello I'm" '.$name;
+?>
